d1d789d8bb329bb5af7b394c70d710a9127eef33
Mirda Ganesh <134841315+mirdaganesh55@users.noreply.github.com>
2024-03-17 10:44:15 +0530

    Delete DynamicWebPages directory

--- DynamicWebPages/src/main/java/com/java/jsf/Dummy.java
+++
@@ -1,9 +0,0 @@
-package com.java.jsf;
-
-public class Dummy {
-	
-	public static void main(String[] args) {
-		PersonDAO dao = new PersonDAOImpl();
-		System.out.println(dao.getAllPersonRecordDao());
-	}
-}
